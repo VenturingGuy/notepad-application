a9a34d39a3051396bf00fa20bfdcc1cf43d1acad
Venturing Guy <LVentura335@Gmail.com>
2022-02-09 01:47:36 -0800

    created function to store individual note objects

--- Assets/Js/script.js
+++ Assets/Js/script.js
@@ -1,12 +1,25 @@
+let notes = [];
+
 updatePage();
 
 function updatePage() {
   if (localStorage.getItem("notepad-title")){
     document.getElementById("user-notepad").innerHTML = localStorage.getItem("notepad-title");
   }
+  if (localStorage.getItem("notes")){
+    notes = JSON.parse(localStorage.getItem("notes"));
+  }
 }
 
 function store() {
-  const inputEmail = document.getElementById("notepad-title");
-  localStorage.setItem("notepad-title", inputEmail.value);
+  const inputNotepad = document.getElementById("notepad-title");
+  localStorage.setItem("notepad-title", inputNotepad.value);
+}
+
+function storeNote() {
+  const noteTitle = document.getElementById("note-title").value;
+  const noteText = document.getElementById("note-text").value;
+  const newNote = {noteTitle, noteText};
+  notes.push(newNote);
+  localStorage.setItem("notes", JSON.stringify(notes));
 }
