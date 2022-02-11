5ac327a6a22f90e5e679cdd695f6edd8200a6e94
Venturing Guy <LVentura335@Gmail.com>
2022-02-10 19:12:33 -0800

    multiple notepads can now be created

--- Assets/Js/script.js
+++ Assets/Js/script.js
@@ -1,7 +1,7 @@
-let notepads = {
-    notepadTitles: [],
+let notepads = [{
+    notepadTitle: "",
     notes: []
-};
+}];
 
 
 updatePage();
@@ -9,44 +9,70 @@ updatePage();
 function updatePage() {
   if (localStorage.getItem("notepads")){
     notepads = JSON.parse(localStorage.getItem("notepads"));
-    document.getElementById("user-notepad").innerText = notepads.notepadTitles[0];
   }
-  displayNotepads();
-  displayNotes();
+  listNotepads();
 }
 
 function storeNotepad() {
   const notepadTitle = document.getElementById("notepad-title").value;
-  console.log(notepads.notepadTitles);
-  notepads.notepadTitles.push(notepadTitle);
+  const newIndex = notepads.length === 1 ? notepads.length - 1 : notepads.length;
+  console.log(notepads);
+  console.log(notepads[newIndex]);
+  let notes = storeNote();
+  notepads[0].notepadTitle === "" ? notepads[0] = {notepadTitle, notes} : notepads.push({notepadTitle, notes});
   localStorage.setItem("notepads", JSON.stringify(notepads));
-  storeNote();
 }
 
 function storeNote() {
-  console.log(notepads.notes)
   const noteTitle = document.getElementById("note-title").value;
   const noteText = document.getElementById("note-text").value;
-  const newNote = {noteTitle, noteText};
-  notepads.notes.push(newNote);
-  localStorage.setItem("notepads", JSON.stringify(notepads));
+  const newNote = [{noteTitle, noteText}];
+  return newNote;
 }
 
-function displayNotepads() {
+function listNotepads() {
   const container = document.querySelector(".notepad__savednotepads");
-
-  notepads.notepadTitles.forEach(function(title){
-    const notepadName = document.createElement("div");
-    notepadName.setAttribute("class", "notepad");
-    notepadName.innerHTML = title;
-    container.appendChild(notepadName);
-  })
+  const button = document.createElement("button");
+  button.setAttribute("class", "notepad__buton");
+  button.innerHTML = "New Notepad";
+  button.onclick = () => {
+    document.querySelector(".notepad__creation").toggleAttribute("hidden");
+    button.innerHTML = (button.innerHTML === "New Notepad" ? button.innerHTML = "Cancel" : button.innerHTML = "New Notepad");
+  }
+  if (localStorage.getItem("notepads")){
+    notepads.forEach(function(notepad, index){
+      const notepadName = document.createElement("div");
+      notepadName.setAttribute("class", "notepad__entry");
+      notepadName.setAttribute("id", `notepad-${index}`);
+      notepadName.onclick = () => {
+        document.getElementById(`saved-${index}`).toggleAttribute("hidden")
+      };
+      notepadName.innerHTML = notepad.notepadTitle;
+      container.appendChild(notepadName);
+      displayNotepad(index);
+    })
+  }
+  container.appendChild(button);
 }
 
-function displayNotes() {
-  const container = document.querySelector(".notepad__saved");
+function displayNotepad(index) {
+  const container = document.getElementById(`notepad-${index}`);
+  console.log(notepads[index].notes);
+  const title=document.createElement("div");
+  title.setAttribute("class", "notepad__saved");
+  title.setAttribute("id", `saved-${index}`);
+  title.setAttribute("hidden", "hidden");
+  container.appendChild(title);
+  displayNotes(index);
+}
 
-  notepads.notes.forEach(function(note){
+function displayNotes(index) {
+  const container = document.getElementById(`saved-${index}`);
+  const currentNotepad = notepads[index];
+  console.log(currentNotepad);
+  console.log(currentNotepad.notes);
+
+  currentNotepad.notes.forEach(note => {
     const noteName = document.createElement("div");
     const noteText = document.createElement("div");
     noteName.setAttribute("class", "notepad__notename");
@@ -55,7 +81,7 @@ function displayNotes() {
     noteText.textContent = note.noteText;
     container.appendChild(noteName);
     container.appendChild(noteText);
-  })
+  });
 }
 
 function deleteNotepad() {
